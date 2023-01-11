0714601702b80a73a54d8087a93916583d301a18
Matt Villa <matthewvilla805@gmail.com>
2023-01-10 23:54:26 -0800

    simplified the db

--- meals/models.py
+++ meals/models.py
@@ -4,10 +4,9 @@ from django.db import models
 
 
 class Recipe(models.Model):
-    image_url = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    recipe_link = models.CharField(max_length=255)
+    recipe_link = models.URLField()
 
     class Meta:
         db_table = 'recipe'
@@ -32,47 +31,16 @@ class User(models.Model):
 class UserGroup(models.Model):
     description = models.CharField(max_length=255)
     recipes = models.ManyToManyField(
-        Recipe, through='GroupRecipe')
+        Recipe)
     ingredients = models.ManyToManyField(
-        Ingredient, through='UserGroupIngredient')
+        Ingredient)
     users = models.ManyToManyField(
-        User, through='GroupUser')
+        User)
     # !need to upate this and set it to another user in the group!
     admin = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='group_admin')
-    
+
     class Meta:
         db_table = 'user_group'
 
 # through models for many to many relationships
-
-
-class GroupUser(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    user_group = models.ForeignKey(UserGroup, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'group_user'
-
-
-class UserGroupIngredient(models.Model):
-    user_group = models.ForeignKey(UserGroup, on_delete=models.CASCADE)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'user_group_ingredient'
-
-
-class RecipeIngredient(models.Model):
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'recipe_ingredient'
-
-class GroupRecipe(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    user_group = models.ForeignKey(UserGroup, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'group_recipe'
