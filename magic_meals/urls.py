5097ed8eeeead28ce033b2aa39804091a9042cff
Matt Villa <matthewvilla805@gmail.com>
2023-01-28 23:48:43 -0800

    changed fields

--- magic_meals/urls.py
+++ magic_meals/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Magic Meals Admin"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('foo/', include('meals.urls')),
--- meals/models.py
+++ meals/models.py
@@ -1,46 +1,60 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Recipe(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    recipe_link = models.URLField()
-
-    class Meta:
-        db_table = 'recipe'
-
-
-class Ingredient(models.Model):
-    title = models.CharField(max_length=255)
-
-    class Meta:
-        db_table = 'ingredient'
-
-
 class User(models.Model):
-    username = models.CharField(max_length=255)
     display_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'user'
 
-
 class UserGroup(models.Model):
-    description = models.CharField(max_length=255)
-    recipes = models.ManyToManyField(
-        Recipe)
-    ingredients = models.ManyToManyField(
-        Ingredient)
-    users = models.ManyToManyField(
-        User)
-    # !need to upate this and set it to another user in the group!
-    admin = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name='group_admin')
+    group_name = models.CharField(max_length=255)
+    description = models.TextField()
+    users = models.ManyToManyField(User)
 
     class Meta:
         db_table = 'user_group'
+
+class Food(models.Model):
+
+    MAJOR_THREAT = 'Major'
+    CAUTION_ONLY = 'Caution'
+    NOT_ASSESSED = 'NA'
+
+    THREAT_LEVELS = [
+        (MAJOR_THREAT, 'Major Threat'),
+        (CAUTION_ONLY, 'Caution only'),
+        (NOT_ASSESSED, 'Not Assesed'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    threat_level = models.CharField(
+        max_length=255,
+        choices=THREAT_LEVELS,
+        default=NOT_ASSESSED,
+    )
+    user_group = models.ForeignKey(UserGroup, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'food'
+
+
+class Allergen(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    major_allergen = models.BooleanField(default=False)
+    user_group = models.ForeignKey(UserGroup, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'allergen'
+
+
+
+
+
+
 
 # through models for many to many relationships
