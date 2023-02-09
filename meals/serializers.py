f03146e041a11b7e096994b7b2fb05a5c0c13938
Matt Villa <matthewvilla805@gmail.com>
2023-02-09 00:35:44 -0800

    group serializer updated

--- meals/serializers.py
+++ meals/serializers.py
@@ -6,6 +6,17 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'display_name', 'email', 'password']
+
+class UserDisplayNameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['display_name']    
+    #! example of a validate override
+    # def validate(self, data):
+    #     if data['password'] != data['confirm_password']:
+    #         return serializers.ValidationError('Passwords no matchy')
+    #     return data
+    #! standard fields for non model serializer    
     # id = serializers.IntegerField()
     # display_name = serializers.CharField(max_length=255)
     # email = serializers.EmailField()
--- meals/views.py
+++ meals/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserDisplayNameSerializer
 
 
 def index(request):
@@ -19,21 +19,30 @@ def all_user_group_list(request):
     return Response('ok')
 
 
-@api_view()
+@api_view(['GET'])
 def all_users_list(request):
-    queryset = User.objects.all()
-    serializer = UserSerializer(queryset, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET', 'POST'])
-def user_detail(request, id):
     if request.method == 'GET':
-        user = get_object_or_404(User, pk=id)
+        queryset = User.objects.all()
+        serializer = UserDisplayNameSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET', 'PUT', 'POST', 'DELETE'])
+def user_detail(request, id):
+    user = get_object_or_404(User, pk=id)
+    if request.method == 'GET':
         serializer = UserSerializer(user)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'POST':
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response('saved user')
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
